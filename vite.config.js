2df15de40fdae5590241e3af96e4fd4430e019fb
faheemali04 <227476587+asimmiqbal04@users.noreply.github.com>
2025-08-21 02:22:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Ayesha-Portfolioi/",
+  base: "/asimPortfolio/",
   server: {
     proxy: {
       '/api': {
