b816081c5572ecedbab7c12a8ad1be13405738ee
faheemali04 <227476587+asimmiqbal04@users.noreply.github.com>
2025-08-21 02:31:26 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/asimPortfolio/',
-})
+  base: "/asimPortfolio/",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://www.zolixi.com', // Zolixi Next.js server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
