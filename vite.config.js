ed21cf5fbb31f94b44d32d4572882b445b1a6823
ayesha <227476587+asimmiqbal04@users.noreply.github.com>
2025-09-10 01:32:01 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // or just remove this line
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.zolixi.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  base: "/", // <-- deploys cleanly on Vercel root
+})
